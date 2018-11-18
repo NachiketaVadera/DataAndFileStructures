320b21ed637cf3190f54bc99f23bdf151b4915a6
NachiketaVadera <personal.nachiketa@gmail.com>
2018-11-19 00:02:58 +0530

    Porgram to convert an expression form infix to postfix (fixed)

--- InfixToPostfix.c
+++ InfixToPostfix.c
@@ -3,54 +3,51 @@
 
 char stack[20];
 int top = -1;
+
 void push(char x) {
-    stack[++top] = x;
+        stack[++top] = x;
 }
 
 char pop() {
-    if(top == -1)
-        return -1;
-    else
-        return stack[top--];
+        if(top == -1)
+                return -1;
+        else
+                return stack[top--];
 }
 
 int priority(char x) {
-    if(x == '(')
-        return 0;
-    if(x == '+' || x == '-')
-        return 1;
-    if(x == '*' || x == '/')
-        return 2;
+        if(x == '(')
+                return 0;
+        if(x == '+' || x == '-')
+                return 1;
+        if(x == '*' || x == '/')
+                return 2;
 }
 
 int main() {
-    char exp[20];
-    char *e, x;
-    printf("Enter the expression :: ");
-    scanf("%s",exp);
-    e = exp;
-    while(*e != '\0')
-    {
-        if(isalnum(*e))
-            printf("%c",*e);
-        else if(*e == '(')
-            push(*e);
-        else if(*e == ')')
-        {
-            while((x = pop()) != '(')
-                printf("%c", x);
+        char expression[20];
+        char *e, x;
+        printf("Enter the expression: \n");
+        scanf("%s",expression);
+        e = expression;
+        while(*e != '\0') {
+                if(isalnum(*e))
+                        printf("%c",*e);
+                else if(*e == '(')
+                        push(*e);
+                else if(*e == ')') {
+                        while((x = pop()) != '(')
+                                printf("%c", x);
+                }
+                else {
+                        while(priority(stack[top]) >= priority(*e))
+                                printf("%c",pop());
+                        push(*e);
+                }
+                e++;
         }
-        else
-        {
-            while(priority(stack[top]) >= priority(*e))
+        while(top != -1)
                 printf("%c",pop());
-            push(*e);
-        }
-        e++;
-    }
-    while(top != -1)
-    {
-        printf("%c",pop());
-    }
-    return 0;
+        printf("\n");
+        return 0;
 }
